3a8d942f7cae62d2501cd4cd7a72f2bf81f57fc3
Patrick Szalewicz <github@fachinformatiker.app>
2019-08-12 12:31:57 +0200

    my script

---
+++ mynewserver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Grundinstallation eines neuen Linux-Servers
+
+cd
+echo "apt update"
+apt update
+echo "apt upgrade"
+apt upgrade -y
+echo "apt install needed software"
+apt install -y dialog nano zip unzip wget curl htop git sudo screen neofetch zsh
+echo "setup neofetch"
+cd .config/neofetch
+mv config.conf config.conf.bak
+wget -O config.conf https://raw.githubusercontent.com/fachinformatiker/dotfiles/master/.config/neofetch/config
+echo "install oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+echo "setup oh my zsh"
+echo "if [ -f /usr/bin/neofetch ]; then neofetch; fi" >> .zshrc
+echo "# source $HOME/.zsh-notify/notify.plugin.zsh" >> .zshrc
+sed -i 's/robbyrussell/geoffgarside/g' .zshrc
+dialog --title 'Server' --msgbox 'Ich habe fertig' 5 20
