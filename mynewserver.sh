0ccd33ceac77315cc4aa7d35edb129629cc2b85b
Patrick Szalewicz <github@fachinformatiker.app>
2019-08-22 12:33:32 +0200

    fixed error

--- mynewserver.sh
+++ mynewserver.sh
@@ -15,7 +15,7 @@ wget -O config.conf https://raw.githubusercontent.com/fachinformatiker/dotfiles/
 echo "install oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 echo "setup oh my zsh"
+cd
 echo "if [ -f /usr/bin/neofetch ]; then neofetch; fi" >> .zshrc
-echo "# source $HOME/.zsh-notify/notify.plugin.zsh" >> .zshrc
 sed -i 's/robbyrussell/geoffgarside/g' .zshrc
 dialog --title 'Server' --msgbox 'Ich habe fertig' 5 20
